a589f8f11b17c69fbba252264de4888a120c4d61
AndrewKust <deradus@ukr.net>
2023-07-16 23:13:10 +0300

    codeStyleFixes

--- KystParallax/Sources/KystParallax/KystParallax.swift
+++ KystParallax/Sources/KystParallax/KystParallax.swift
@@ -6,12 +6,15 @@ import CoreMotion
 #if os(iOS)
 public struct ParallaxLayer:View {
     @ObservedObject var manager = MotionManager()
+    
     var image:Image
     var magnitude:Int
+    
     var body: some View {
         image
             .modifier(ParallaxMotionModifier(manager: manager, magnitude: magnitude ))
     }
+    
     init(image: Image, magnitude: Int) {
         self.image = image
         self.magnitude = magnitude
@@ -19,7 +22,6 @@ public struct ParallaxLayer:View {
 }
 
 struct ParallaxMotionModifier: ViewModifier {
-    
     @ObservedObject var manager: MotionManager
     var magnitude: Double
     
@@ -30,7 +32,6 @@ struct ParallaxMotionModifier: ViewModifier {
 }
 
 class MotionManager: ObservableObject {
-    
     @Published var pitch: Double = 0.0
     @Published var roll: Double = 0.0
     
@@ -50,16 +51,19 @@ class MotionManager: ObservableObject {
                 self.roll = motionData.attitude.roll
             }
         }
-        
     }
 }
+
 #elseif os(macOS)
+
 @available(macOS 10.15, *)
 public struct ParallaxLayer:View {
     var image:Image
+    
     @State private var xOffset: CGFloat = 0
     @State private var yOffset: CGFloat = 0
-    public var body: some View{
+    
+    public var body: some View {
         image
             .resizable()
             .aspectRatio(contentMode: .fill)
@@ -70,14 +74,18 @@ public struct ParallaxLayer:View {
                     let mouseLocation = event.locationInWindow
                     let windowLocation = NSApp.windows[0].frame.origin
                     let mouseInView = CGPoint(x: mouseLocation.x - windowLocation.x, y: mouseLocation.y - windowLocation.y)
+                    
                     xOffset = ((NSScreen.main!.frame.width / 2) - mouseInView.x) / 50
                     yOffset = (mouseInView.y - (NSScreen.main!.frame.height / 2)) / 50
+                    
                     return event
                 }
             }
     }
+    
     init(image: Image) {
         self.image = image
     }
 }
+
 #endif
