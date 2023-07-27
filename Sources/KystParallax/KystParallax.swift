28360e24e803c6c705baf606fce5edff867417a1
AndrewKust <deradus@ukr.net>
2023-07-28 02:42:40 +0300

    MacOs debug

--- Sources/KystParallax/KystParallax.swift
+++ Sources/KystParallax/KystParallax.swift
@@ -58,31 +58,61 @@ public class MotionManager: ObservableObject {
 
 @available(macOS 10.15, *)
 public struct ParallaxLayer: View {
-    var image: Image
-    var speed: CGFloat
+    let image: Image
+    let speed: CGFloat
     
-    @State private var xOffset: CGFloat = 0
-    @State private var yOffset: CGFloat = 0
+    @State private var absXOffset: CGFloat = 0
+    @State private var absYOffset: CGFloat = 0
+    
+    @State private var isMouseInside: Bool = false
+    @State private var previousMousePosition: CGPoint = .zero
     
     public var body: some View {
         image
-            .offset(x: xOffset, y: yOffset)
-            .onAppear {
-                NSEvent.addLocalMonitorForEvents(matching: .mouseMoved) { event in
-                    let mouseLocation = event.locationInWindow
-                    
-                    guard mouseLocation.x > 0 && mouseLocation.y > 0 else { return event }
-                    
-                    let windowLocation = NSApp.windows[0].frame.origin
-                    
-                    let mouseInView = CGPoint(x: mouseLocation.x - windowLocation.x, y: mouseLocation.y - windowLocation.y)
-                    
-                    xOffset = (((NSScreen.main!.frame.width / 2) - mouseInView.x) / 50 ) * speed
-                    yOffset = ((mouseInView.y - (NSScreen.main!.frame.height / 2)) / 50 ) * speed
-                    
-                    return event
-                }
+            .offset(x: absXOffset  , y: absYOffset )
+            .animation(.easeInOut(duration: 0.1), value: absYOffset) //Y changed after X and we can play smooth animation
+            .onAppear { subscribeParallaxAnim() }
+    }
+    
+    private func subscribeParallaxAnim() {
+        NSEvent.addLocalMonitorForEvents(matching: [.mouseMoved, .mouseEntered, .mouseExited]) { event in
+            guard let window = NSApp.windows.first(where: { $0.isVisible }),
+                  let contentView = window.contentView else {
+                return event
             }
+            
+            let mouseLocation = event.locationInWindow
+            let mouseInView = contentView.convert(mouseLocation, from: nil)
+            
+            if event.type == .mouseEntered {
+                isMouseInside = true
+            } else if event.type == .mouseExited {
+                isMouseInside = false
+            }
+            
+            if isMouseInside {
+                let windowRect = window.frame
+                let windowOrigin = windowRect.origin
+                
+                let windowCenter = NSPoint(x: windowOrigin.x + windowRect.width / 2, y: windowOrigin.y + windowRect.height / 2)
+                
+                let mousePosRelatedToWndCenter = NSPoint(x: mouseInView.x - windowCenter.x, y: mouseInView.y - windowCenter.y)
+                
+                // -1...1 * speed
+                let deltaXRel = (mouseInView.x / windowRect.width - 0.5) * 2 * speed
+                let deltaYRel = (mouseInView.y / windowRect.height - 0.5) * 2 * speed
+                
+                let deltaFinalX = (deltaXRel - absXOffset)
+                let deltaFinalY = (deltaYRel - absYOffset)
+                
+                absXOffset += deltaFinalX
+                absYOffset += deltaFinalY
+                
+                previousMousePosition = mousePosRelatedToWndCenter
+            }
+            
+            return event
+        }
     }
     
     public init(image: Image, speed: CGFloat = 1) {
